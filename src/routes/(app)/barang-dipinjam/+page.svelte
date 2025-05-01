0fc17385826f0b9f77ab584aae5044fdad60422d
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-01 08:26:39 +0700

    oke

--- src/routes/(app)/barang-dipinjam/+page.svelte
+++ src/routes/(app)/barang-dipinjam/+page.svelte
@@ -76,12 +76,12 @@
 
   <!-- Display Success/Error Messages -->
   {#if successMessage}
-    <div in:fly={{ y: -10, duration: 300 }} class="bg-green-100 text-green-800 px-4 py-2 rounded">
+    <div in:fly={{ y: -10, duration: 300 }} class="bg-green-100 text-green-800 mt-4 px-4 py-4 rounded">
       {successMessage}
     </div>
   {/if}
   {#if errorMessage}
-    <div in:fly={{ y: -10, duration: 300 }} class="bg-red-100 text-red-800 px-4 py-2 rounded">
+    <div in:fly={{ y: -10, duration: 300 }} class="bg-red-100 text-red-800 mt-4 px-4 py-2 rounded">
       {errorMessage}
     </div>
   {/if}

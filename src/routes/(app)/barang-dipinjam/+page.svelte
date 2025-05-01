8f1902890b76aec11d190aadbf69f9f6493d0902
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-01 07:39:10 +0700

    oke

--- src/routes/(app)/barang-dipinjam/+page.svelte
+++ src/routes/(app)/barang-dipinjam/+page.svelte
@@ -54,7 +54,7 @@
 </script>
 
 
-<main class="max-w-4xl mx-auto mt-10 p-6 bg-white rounded-2xl shadow space-y-6">
+<main class="max-w-4xl mx-auto mt-10 p-6">
 
   <h1 class="text-2xl font-bold text-gray-800">Daftar Barang yang Dipinjam</h1>
 

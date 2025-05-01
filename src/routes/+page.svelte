6119ec77b94308c905c47da95b619c16dd164817
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-01 09:38:51 +0700

    oke

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,17 +83,17 @@
       </div>
 
       <button
-    type="submit"
-    class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition flex items-center justify-center"
-    disabled={isLoading}
-  >
-    {#if isLoading}
-      <svg class="animate-spin h-5 w-5 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
-      </svg>
-      Loading...
-    {:else}
+        type="submit"
+        class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition flex items-center justify-center"
+        disabled={isLoading}
+      >
+      {#if isLoading}
+        <svg class="animate-spin h-5 w-5 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
+        </svg>
+        Loading...
+      {:else}
       Masuk
     {/if}
   </button>

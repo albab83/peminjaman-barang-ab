37a345a9461e1286fd353ff8adfead8aacc5faba
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-05 05:52:31 +0700

    Update +page.svelte

--- src/routes/(app)/peminjaman/+page.svelte
+++ src/routes/(app)/peminjaman/+page.svelte
@@ -169,7 +169,7 @@
 
   const fetchPeminjam = async () => {
     try {
-      const res = await axios.get('https://backend-peminjaman-barang-production.up.railway.app/api/peminjaman/sedang-dipinjam', {
+      const res = await axios.get('https://backend-peminjaman-barang-production.up.railway.app/api/peminjaman/barang-dipinjam', {
         headers: { Authorization: `Bearer ${token}` }
       });
       peminjamList = res.data?.data ?? [];

fbe91bee519a67c4384e0824fec314065d8e1f35
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-06 14:39:31 +0700

    Update +page.svelte

--- src/routes/(app)/peminjaman/+page.svelte
+++ src/routes/(app)/peminjaman/+page.svelte
@@ -125,7 +125,7 @@
         type="text"
         bind:value={peminjam}
         placeholder="Masukkan nama"
-        class="w-full border rounded px-3 py-2"
+        class="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
 

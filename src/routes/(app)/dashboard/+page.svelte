d5f4424519221ed6db87b46a6325f521117a4202
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-08 15:32:38 +0700

    Update +page.svelte

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -294,7 +294,7 @@
             <h3 class="text-lg font-medium text-gray-900">Barang Sedang Dipinjam</h3>
             <p class="mt-1 text-sm text-gray-500">Lihat peminjaman barang</p>
             <div class="mt-4">
-              <a href="/riwayat" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
+              <a href="/dipinjam" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>                

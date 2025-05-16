1db7d2c4321325eb4f1fbf4b5fb9ea1331054f8c
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-16 08:52:17 +0700

    ubah tranparansi

--- src/routes/(app)/tambah-barang/+page.svelte
+++ src/routes/(app)/tambah-barang/+page.svelte
@@ -264,7 +264,7 @@
 
   <!-- form moadal -->
   {#if showFormModal}
-    <div class="fixed inset-0 bg-black bg-opacity/50 flex justify-center items-center z-50">
+    <div class="fixed inset-0 bg-black/40 flex justify-center items-center z-50">
       <div class="bg-white rounded-xl shadow-lg w-full max-w-md p-6 relative">
         <button
           class="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
